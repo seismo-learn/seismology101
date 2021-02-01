8188bd42f5dde4f1df9d3fbcc7f1cfa2083e974e
core-man <coreman.seism@gmail.com>
2021-02-01 22:47:50 +0800

    Add a bash script for rsync

---
+++ source/best-practices/backup.sh
@@ -0,0 +1,57 @@
+#!/usr/bin/env bash
+#
+# 使用 rsync 命令备份指定目录
+#
+
+source="${HOME}/workspace/source"  # 源目录
+backup="${HOME}/workspace/backup"  # 备份目录
+log="${backup}/backup.log"         # 备份目录下的备份日志
+
+mkdir -p "${backup}"      # 若备份目录不存在，则新建
+
+dirs=( ${source}/* )      # 备份家目录下所有子目录（不含隐藏目录和文件）
+# dirs=(                  # 仅备份部分目录
+#     "${source}/src"
+#     "${source}/software"
+#     "${source}/codes"
+# )
+
+# 备份开始时间
+start=`date +%F-%H:%M:%S`
+echo "## backup begins at ${start}" >> "${log}"
+
+# 按序备份每个目录
+for dir in "${dirs[@]}"; do
+    echo -e "------------------------------------------\nbackup ${dir}"
+    now=`date +%F-%H:%M:%S`
+	echo "backup ${dir} at ${now}" >> "${log}"
+    rsync -av --delete "${dir}" "${backup}"
+done
+
+# 备份结束时间
+end=`date +%F-%H:%M:%S`;
+echo -e "## backup ends at ${end}\n\n" >> "${log}"
+
+# 检查备份目录下是否存在源目录已删除的目录
+echo -e "\n++++++++++++++++++++++++++++++++++++\nBackup is finished! Begin to check!\n"
+flag=1
+dirs_backup=( ${backup}/* )
+for dir_backup in "${dirs_backup[@]}"; do
+    # 忽略备份日志
+    dir_name=$(basename "${dir_backup}")
+    if [ "${dir_backup}" = "${log}" ]; then
+        continue
+    fi
+
+    # 备份目录下存在源目录已删除的目录
+    if [[ ! "${dirs[@]}"  =~ "${source}/${dir_name}" ]]; then
+        echo "[${dir_name}] is deleted in [${source}] but is still in [${backup}]."
+        flag=0
+    fi
+done
+
+if [ ${flag} = 0 ]; then
+    echo "You may choose to delete them in [${backup}] later."
+else
+    echo "Backup is successful!"
+fi
