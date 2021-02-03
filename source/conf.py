8239d66446a1e72aabdacd4da394652e6c4bc31d
Dongdong Tian <seisman.info@gmail.com>
2021-02-02 20:28:29 -0500

    Add the sphinx-panels extension (#155)
    Co-authored-by: Jiayuan Yao <coreman.seism@gmail.com>

--- source/conf.py
+++ source/conf.py
@@ -47,6 +47,7 @@ extensions = [
     "sphinx_cjkspace.cjkspace",
     "sphinx_copybutton",
     "sphinx_tabs.tabs",
+    "sphinx_panels",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
