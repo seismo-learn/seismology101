cfd29c6d5a8e6a71453a6300930fc1ccff04a072
Jiayuan Yao <coreman.seism@gmail.com>
2021-02-04 15:11:03 +0800

    Update source/best-practices/backup.bat

--- source/best-practices/backup.bat
+++ source/best-practices/backup.bat
@@ -15,7 +15,8 @@ REM robocopy 命令常用选项：
 REM   /mir        : 备份目录成为镜像 （复制子目录；删除备份目录下源目录中不存在的文件和目录）
 REM   /mt[:n]     : 使用 n 个线程进行多线程复制（默认值为 8）。n 至少为 1，但不得大于 128
 REM   /log+:file  : 将备份状态以追加的方式输出到日志文件 file 中
-REM 可以参考 http://www.svngo.net/article61 了解 robocopy 命令更多用法
+REM 可以参考 https://docs.microsoft.com/zh-cn/windows-server/administration/windows-commands/robocopy 了解 robocopy 命令更多用法
+
 
 robocopy d:\directory1 e:\backup\directory1 /mir /mt:16 /log+:e:\backup.txt
 robocopy d:\directory2 e:\backup\directory2 /mir /mt:16 /log+:e:\backup.txt
