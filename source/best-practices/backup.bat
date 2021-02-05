15a67b2dd39735e00c514a1de12102bce63e53cc
core-man <coreman.seism@gmail.com>
2021-02-05 15:16:25 +0800

    Change Windows disk to upper case

--- source/best-practices/backup.bat
+++ source/best-practices/backup.bat
@@ -1,6 +1,6 @@
 REM
 REM  目的： 使用 robocopy 命令备份指定目录
-REM  作者： 李天觉
+REM  作者： 李天觉、赵志远
 REM  最近修改日期： 2021年2月4日
 REM
 REM  免责声明：
@@ -9,9 +9,9 @@ REM    并不具备普遍适用性，仅供读者参考。
 REM
 
 REM 源目录和备份目录
-REM   d:\directory1 和 d:\directory2 : 要备份的源目录
-REM   e:\backup\      : 备份目录
-REM   e:\backup.txt   : 备份目录下的备份日志
+REM   D:\directory1 和 D:\directory2 : 要备份的源目录
+REM   E:\backup\      : 备份目录
+REM   E:\backup.txt   : 备份目录下的备份日志
 
 REM robocopy 命令常用选项：
 REM   /mir        : 备份目录成为镜像 （复制子目录；删除备份目录下源目录中不存在的文件和目录）
@@ -24,10 +24,10 @@ REM   1. 如果命令中有中文，需要将脚本另存为成 ANSI 编码格�
 REM      本脚本是 Linux 下采用 UTF-8 格式编写的。
 REM   2. 如果文件夹的名字有空格，必须加引号
 
-# 备份 d:\directory1 到 e:\backup\directory1\
-robocopy "d:\directory1" "e:\backup\directory1\" /mir /mt /log+:"e:\backup.txt"
+# 备份 D:\directory1 到 E:\backup\directory1\
+robocopy "D:\directory1" "E:\backup\directory1\" /mir /mt /log+:"E:\backup.txt"
 
-# 备份 d:\directory2 到 e:\backup\directory2\
-robocopy "d:\directory2" "e:\backup\directory2\" /mir /mt /log+:"e:\backup.txt"
+# 备份 D:\directory2 到 E:\backup\directory2\
+robocopy "D:\directory2" "E:\backup\directory2\" /mir /mt /log+:"E:\backup.txt"
 
 pause
