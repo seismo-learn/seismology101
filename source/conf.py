2cba9a04284442da540d1cb87efba47741ade25a
core-man <coreman.seism@gmail.com>
2021-06-01 16:01:31 +0800

    Install npsphinx

--- source/conf.py
+++ source/conf.py
@@ -47,6 +47,7 @@ extensions = [
     "sphinx_cjkspace.cjkspace",
     "sphinx_copybutton",
     "sphinx_panels",
+    "nbsphinx",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
